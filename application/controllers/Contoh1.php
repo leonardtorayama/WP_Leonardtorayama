14feec64ac4ced6d4b224bff64363b993f49bdaf
leonardtorayama <jahpepet666@gmail.com>
2022-04-24 18:09:49 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan<h1>";
+        echo"Nama saya Leonard Torayama Salombe
+            Saya tinggal di Bogor
+            Main game adalah kesukaan saya sampai melupakan tugas web programming 2";
+    }
+}
---
+++ application/controllers/Latihan1.php
@@ -0,0 +1,8 @@
+<?php
+class latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Selamat datang.... Selamat belajar Web Programming"
+    }
+}
